d00085db0dec5d052c4d073862061842f30d1237
Linn <ariana.mercado@utec.edu.pe>
2024-06-22 17:46:43 -0500

    home2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './Home.css';
 import amazonLogo from './amazon.png';
+import carritoImg from './carrito.png';
 
 const Home = () => {
+
+
     return (
         <div>
             <header>
@@ -10,6 +13,7 @@ const Home = () => {
                     <ul>
                         <li><a href="/login">Login</a></li>
                         <li><a href="/register">Register</a></li>
+                        <li><a href="/carrito"><img src={carritoImg} alt="Carrito" className="carrito"/></a></li>
                     </ul>
                 </nav>
                 <img src={amazonLogo} alt="Amazon Logo" className="decoration-line"/>
