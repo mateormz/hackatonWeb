b777f6156cbf06f8e4b5d22191ae7e405e84627e
Linn <ariana.mercado@utec.edu.pe>
2024-06-22 17:33:19 -0500

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import './Home.css';
+import amazonLogo from './amazon.png';
 
 const Home = () => {
-  return (
-    <div>
-      CLIENT
-    </div>
-  )
+    return (
+        <div>
+            <header>
+                <nav>
+                    <ul>
+                        <li><a href="/login">Login</a></li>
+                        <li><a href="/register">Register</a></li>
+                    </ul>
+                </nav>
+                <img src={amazonLogo} alt="Amazon Logo" className="decoration-line"/>
+            </header>
+        </div>
+    )
 }
 
-export default Home
+export default Home;
