bcf716ed4f1104b70aa18d12c2e356838ea128fa
Sebastian Alonso Caycho Meza <sebastian.caycho.m@utec.edu.pe>
2024-06-22 16:23:33 -0500

    sebas2

--- src/services/api.js
+++ src/services/api.js
@@ -22,20 +22,86 @@ export const fetchRegister = async(username, password, role) => {
     return response.data;
 }
 
-export const getPassenger = async () => {
-    const token = localStorage.getItem('token');
-      const response = await axios.get(`${BACKEND_URL}/passenger/me`, {
-        headers: { Authorization: `Bearer ${token}` }
-      });
-      return response.data;
-};
-
-export const fetchRides = async(page) => {
-    const response = await axios.get(`${BACKEND_URL}/ride/user?page=${page}&size=10`, {
+const createItem = async (data, token) => {
+    try {
+      const response = await axios.post(`${BACKEND_URL}/items`, data, {
         headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`,
-        },
-    });
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error al crear el item:', error.response);
+      return null;
+    }
+  };
+
+  const editItem = async (itemData, token) => {
+    try {
+      const response = await axios.put(`${BACKEND_URL}/items/${itemData.itemId}`, {
+        boughtInLastMonth: itemData.boughtInLastMonth,
+        imgUrl: itemData.imgUrl,
+        isBestSeller: itemData.isBestSeller,
+        price: itemData.price,
+        stars: itemData.stars,
+        title: itemData.title
+      }, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error al editar el item:', error.response);
+      return null;
+    }
+  };
+
+  const deleteItem = async (itemId, token) => {
+    try {
+      const response = await axios.delete(`${BACKEND_URL}/items/${itemId}`, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error al eliminar el item:', error.response);
+      return null;
+    }
+  };
+
+  const getItem = async (itemId, token) => {
+    try {
+      const response = await axios.get(`${BACKEND_URL}/item/${itemId}`, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error al obtener el item:', error.response);
+      return null;
+    }
+  };
+
+  const getItems = async (limit, lastKey = null) => {
+    try {
+      const response = await axios.get(`${BACKEND_URL}/items`, {
+        params: {
+          limit: limit,
+          lastKey: lastKey
+        }
+      });
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error al obtener los items:', error.response);
+      return null;
+    }
+  };
     
-    return response;
-}
